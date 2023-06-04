3a4af2378c4eef34289900edcf4b901d1e61fa53
Prezes <baniewiczszymonn@gmail.com>
2023-06-04 17:09:55 +0200

    Podstawowa struktura strony

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Real Estate Company</title>
+</head>
+<body>
+    <header>  <!-- naglowek -->
+        <h1>Real Estate Company</h1>
+        <nav>
+            <ul> <!-- przyciski pod napisem -->
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contact</a></li>
+                <li><a href="#">Login</a></li>
+                <li><a href="#">Sign up</a></li>
+            </ul>
+        </nav>
+    </header>
+    <section class="search-container"> <!-- tlo sekcji do szukania -->
+        <section class="search-section"> <!-- sekcja do szukania -->
+            <div class="search-buttons"> <!-- przyciski -->
+                <button class="active">Buy</button>
+                <button>Rent</button>
+            </div>
+            <div class="search-bar"> <!-- pole do szukania -->
+                <input type="text" placeholder="Search for your dream property | Enter city">
+                <button>Search</button>
+            </div>
+        </section>
+    </section>
+    <section class="content-section"> <!-- sekcja opisowa strony -->
+
+        Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...<br>
+        Curabitur et arcu vel orci vehicula sodales at sit amet nisl. Mauris ultricies varius pellentesque
+        
+    </section>
+    <footer> <!-- Stopka -->
+        <p>&copy; 2023 Szymon Baniewicz - WPRG Project.</p>
+    </footer>
+</body>
+</html>
